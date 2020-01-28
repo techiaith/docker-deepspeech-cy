8f40a7ed3d5a7419ca4e7e8462569834698497e0
Dewi Bryn Jones <d.b.jones@bangor.ac.uk>
2020-01-28 13:36:45 +0000

    twtio

--- local/bin/run-tl-cv-lleisiwr.sh
+++ local/bin/run-tl-cv-lleisiwr.sh
@@ -5,18 +5,20 @@ if [ -n "${SINGULARITY_CONTAINER}" ]; then
 fi
 
 checkpoint_dir=$(python -c 'from xdg import BaseDirectory as xdg; print(xdg.save_data_path("deepspeech/gwion-tl-cv-lleisiwr"))')
+export_dir=/export/gwion-tl-cv-lleisiwr
+summary_dir=/keep/transfer/summaries
 
-echo $checkpoint_dir
-
-rm -rf /export/tl-cv-lleisiwr
+rm -rf $checkpoint_dir
+rm -rf $export_dir
+rm -rf $summary_dir
 
 python -u /DeepSpeech/DeepSpeech.py \
-	--train_files /data/commonvoice-cy/deepspeech.csv \
-	--test_files /data/corpws_lleisiwr/corpws_gwion/deepspeech.csv \
-	--alphabet_config_path bin/bangor_welsh/alphabet.txt \
+	--train_files  /data/commonvoice-cy/deepspeech.csv,/data/corpws_lleisiwr/corpws_gwion/train_1.csv \
+	--test_files /data/corpws_lleisiwr/corpws_gwion/test_1.csv \
+	--alphabet_config_path /DeepSpeech/bin/bangor_welsh/alphabet.txt \
 	--lm_binary_path /data/corpws_lleisiwr/corpws_gwion/lm.binary \
 	--lm_trie_path /data/corpws_lleisiwr/corpws_gwion/trie \
-	--source_model_checkpoint_dir /checkpoints/mozilla/deepspeech-0.5.1-checkpoint \
+        --source_model_checkpoint_dir /checkpoints/mozilla/deepspeech-0.5.1-checkpoint \
 	--nofine_tune \
 	--epochs 10 \
 	--drop_source_layers 1 \
@@ -24,17 +26,7 @@ python -u /DeepSpeech/DeepSpeech.py \
 	--test_batch_size 48 \
 	--dev_batch_size 48 \
 	--checkpoint_dir "$checkpoint_dir" \
-	--summary_dir /keep/transfer/summaries \
-	--export_dir /export/tl-cv-lleisiwr \
+	--summary_dir "$summary_dir" \
+	--export_dir "$export_dir" \
 	"$@"
-
-
-#cp bin/bangor_welsh/alphabet.txt /export/CofnodYCynulliad
-
-#cp /data/corpws_tts/trie /export/CofnodYCynulliad
-#cp /data/corpws_tts/lm.binary /export/CofnodYCynulliad
-
-#convert_graphdef_memmapped_format --in_graph=/export/macsen/output_graph.pb --out_graph=/export/macsen/output_graph.pbmm
-
-#!/bin/bash
 
