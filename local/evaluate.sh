b4efe4e843ec551c13cb666532188eded0d71510
Dewi Bryn Jones <d.b.jones@bangor.ac.uk>
2021-01-27 13:36:35 +0000

    cywiriadau evaluate.sh corrections

--- local/evaluate.sh
+++ local/evaluate.sh
@@ -3,7 +3,7 @@ set -e
 set -u
 set -o pipefail
 
-testset_dir=''
+testset_path=''
 scorer_path=''
 
 alphabet_file_path=/DeepSpeech/bin/bangor_welsh/alphabet.txt
@@ -12,19 +12,19 @@ checkpoint_cy_dir=/checkpoints/cy
 while getopts ":t:s:" opt; do
   case $opt in    
     t)
-        testset_dir=$OPTARG
+        testset_path=$OPTARG
         ;;
-	  s)
-		    scorer_path=$OPTARG
-		    ;;    
+    s)
+        scorer_path=$OPTARG
+	;;    
     \?) echo "Invalid option -$OPTARG" >&2
     ;;
   esac
 done
 shift "$(($OPTIND -1))"
 
-if [ -z "$testset_dir" ]; then
-    echo "-t testset_dir not set (csv file containing speech test set)"
+if [ -z "$testset_path" ]; then
+    echo "-t testset_path not set (path to csv file containing speech test set)"
    	exit 2
 fi
 if [ -z "$scorer_path" ]; then
@@ -40,8 +40,8 @@ echo "##########################################################################
 set -x
 
 python -u /DeepSpeech/evaluate.py \
-	--test_files "${testset_dir}/data/trawsgrifio/OpiwHxPPqRI/deepspeech.csv" \
-  --test_batch_size 1 \
+	--test_files "${testset_path}" \
+	--test_batch_size 1 \
 	--alphabet_config_path "${alphabet_file_path}" \
 	--load_checkpoint_dir "${checkpoint_cy_dir}" \
 	--scorer_path "${scorer_path}"
